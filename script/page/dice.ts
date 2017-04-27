daa6a400666cda43fc732351d2f666ded5bf6e7b
F_ <hn.fspace@outlook.com>
2017-04-27 18:57:44 +0900

    カスタムダイスの実装

--- script/page/dice.ts
+++ script/page/dice.ts
@@ -13,12 +13,16 @@ namespace Cthulhu {
 			}
 		}());
 
-		const modes = document.querySelectorAll("#dice>.mode-list>.mode");
+		const modes = document.querySelectorAll("#dice>.contents>.mode-list>.mode");
 		for (const mode of <NodeListOf<HTMLElement>>modes) {
 			const id = mode.dataset["dice"];
 			if (id != null) {
-				manager.register(id, DiceSet.parse(id));
-				mode.addEventListener("click", () => { manager.select(id); });
+				if (id === 'custom') {
+					mode.addEventListener("click", () => { openCustomDiceDialog(); });
+				} else {
+					manager.register(id, DiceSet.parse(id));
+					mode.addEventListener("click", () => { manager.select(id); });
+				}
 				callbacks.push(() => {
 					if (manager.current === id) {
 						mode.classList.add(SELETED_CLASS);
@@ -31,14 +35,14 @@ namespace Cthulhu {
 	}
 
 	function initRollButton(): void {
-		const rollButton = document.querySelector("#dice>.roll-button");
+		const rollButton = document.querySelector("#dice>.contents>.roll-button");
 		if (rollButton instanceof HTMLButtonElement) {
 			rollButton.addEventListener("click", () => { manager.roll() });
 		}
 	}
 
 	function initRollSound(): void {
-		const rollSound = document.querySelector("#dice>audio");
+		const rollSound = document.querySelector("#dice>.contents>audio");
 		if (rollSound instanceof HTMLAudioElement) {
 			manager.addListener(new class {
 				public onRoll(manager: DiceManager, type: DiceRollEventType) {
@@ -53,7 +57,7 @@ namespace Cthulhu {
 	}
 
 	function initDiceView(): void {
-		const diceView = document.querySelector("#dice>.result>.dice-view");
+		const diceView = document.querySelector("#dice>.contents>.result>.dice-view");
 		const diceImage = document.getElementById("dice-image");
 		if (diceView instanceof HTMLElement && diceImage instanceof HTMLImageElement) {
 			const renderer = new DiceRenderer(diceView, new DiceImage(diceImage));
@@ -63,11 +67,69 @@ namespace Cthulhu {
 	}
 
 	function initNumberView(): void {
-		const numberView = document.querySelector("#dice>.result>.number-view");
+		const numberView = document.querySelector("#dice>.contents>.result>.number-view");
 		if (numberView instanceof HTMLElement) {
 			const renderer = new DiceNumberRenderer(numberView);
 
 			manager.addListener(renderer);
+		}
+	}
+
+	function initCustomDiceDialog(): void {
+		const cancelButton = document.querySelector("#dice>.overlay>.custom-dice-dialog .cancel");
+		const okButton = document.querySelector("#dice>.overlay>.custom-dice-dialog .ok");
+		if (cancelButton instanceof HTMLButtonElement) {
+			cancelButton.addEventListener("click", () => { closeCustomDiceDialog(true); });
+		}
+		if (okButton instanceof HTMLButtonElement) {
+			okButton.addEventListener("click", () => { closeCustomDiceDialog(false); });
+		}
+	}
+
+	function openCustomDiceDialog(): void {
+		const dialog = document.querySelector("#dice>.overlay>.custom-dice-dialog");
+		if (dialog instanceof HTMLElement) {
+			dialog.classList.remove('hidden');
+		}
+
+		updateOverlay();
+	}
+
+	function closeCustomDiceDialog(cancel: boolean): void {
+		const dialog = document.querySelector("#dice>.overlay>.custom-dice-dialog");
+		if (dialog instanceof HTMLElement) {
+			dialog.classList.add('hidden');
+
+			if (!cancel) {
+				const countInput = document.querySelector("#dice>.overlay>.custom-dice-dialog .count");
+				const maxInput = document.querySelector("#dice>.overlay>.custom-dice-dialog .max");
+				if (countInput instanceof HTMLInputElement && maxInput instanceof HTMLInputElement) {
+					if (countInput.validity.valid && maxInput.validity.valid) {
+						const id = 'custom';
+						const diceSet = DiceSet.create(countInput.valueAsNumber, maxInput.valueAsNumber);
+
+						manager.register(id, diceSet);
+						manager.select(id);
+					}
+				}
+			}
+		}
+
+		updateOverlay();
+	}
+
+	function updateOverlay(): void {
+		const contents = document.querySelector("#dice>.contents");
+		const overlay = document.querySelector("#dice>.overlay");
+		if (contents instanceof HTMLElement && overlay instanceof HTMLElement) {
+			const children = Array.from(overlay.children);
+			if (children.some(dialog => !dialog.classList.contains('hidden'))) {
+				contents.classList.add('disabled');
+				overlay.classList.remove('disabled');
+			} else {
+				contents.classList.remove('disabled');
+				overlay.classList.add('disabled');
+			}
 		}
 	}
 
@@ -77,5 +139,6 @@ namespace Cthulhu {
 		initRollSound();
 		initDiceView();
 		initNumberView();
+		initCustomDiceDialog();
 	})
 }
