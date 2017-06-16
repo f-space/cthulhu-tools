6833e41568a5839427db4878f4e1af9672b86c39
F_ <hn.fspace@outlook.com>
2017-06-16 20:50:15 +0900

    ステータスページの簡易実装

--- ts/page/root.ts
+++ ts/page/root.ts
@@ -1,7 +1,13 @@
 import page from "./page";
+import storage from "./storage";
+
 import dice from "./dice";
+import status from "./status";
 
 document.addEventListener("DOMContentLoaded", () => {
 	page();
+	storage();
+
 	dice();
+	status();
 });
