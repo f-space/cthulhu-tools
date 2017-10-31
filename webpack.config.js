1096e07fdd0a9e8a2ce213e7a2570bf25cdb3a59
F_ <hn.fspace@outlook.com>
2017-10-31 18:19:44 +0900

    webpack中心の構成へと変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,48 @@
-const path = require("path");
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { LoaderOptionsPlugin } = require('webpack');
 
 module.exports = {
-	entry: "./js/page/root.js",
+	entry: {
+		css: "./scss/main.scss",
+		js: "./ts/page/root.ts"
+	},
 	output: {
-		filename: "./main.js"
+		path: path.resolve(__dirname, "docs"),
+		filename: "main.[name]"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				loader: ExtractTextPlugin.extract("css-loader?-url!sass-loader")
+			},
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				loader: "ts-loader"
+			}
+		]
 	},
 	resolve: {
+		extensions: [".ts", ".js", ".json"],
 		modules: [
-			path.resolve("./js"),
+			path.resolve(__dirname, "ts"),
 			"node_modules"
 		]
 	},
+	devServer: {
+		contentBase: "docs"
+	},
+	plugins: [
+		new ExtractTextPlugin("main.css"),
+	]
+}
+
+if (!process.argv.includes("--debug")) {
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new UglifyJSPlugin(),
+		new LoaderOptionsPlugin({ minimize: true })
+	]);
 }
