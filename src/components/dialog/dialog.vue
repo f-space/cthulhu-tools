a34697591c3247a2ebaafc3f0ece2cd74b63aaa4
F_ <hn.fspace@outlook.com>
2017-11-21 01:13:16 +0900

    ページとダイアログをコンポーネントとして分離

---
+++ src/components/dialog/dialog.vue
@@ -0,0 +1,3 @@
+<template lang="pug" src="./dialog.pug"></template>
+<script lang="ts" src="./dialog.ts"></script>
+<style lang="scss" src="./dialog.scss" scoped></style>
---
+++ src/components/page/page.vue
@@ -0,0 +1,2 @@
+<template lang="pug" src="./page.pug"></template>
+<style lang="scss" src="./page.scss" scoped></style>
