1a16d2b0f159647032ad6a99d4a97e61bac265e4
F_ <hn.fspace@outlook.com>
2017-11-23 18:16:23 +0900

    XMLHttpRequestをfetchで置き換えるリファクタリング

--- src/components/resource/resource.ts
+++ src/components/resource/resource.ts
@@ -23,27 +23,12 @@ export default class AppResource extends Vue {
 		this.diceSound = this.$refs.diceSound as HTMLAudioElement;
 
 		const url = this.diceImage.dataset.layout as string;
-		this.requestJson(url).then(data => {
-			this.diceImageLayout = data;
-		});
-	}
-
-	private requestJson(url: string): Promise<any> {
-		return new Promise((resolve, reject) => {
-			const xhr = new XMLHttpRequest();
-			xhr.responseType = 'json';
-			xhr.open('GET', url);
-			xhr.onload = () => {
-				if (xhr.status === 200) {
-					resolve(xhr.response);
-				} else {
-					reject(new Error(xhr.statusText));
-				}
-			};
-			xhr.onerror = () => {
-				reject(new Error(xhr.statusText));
-			};
-			xhr.send();
+		fetch(url).then(response => {
+			if (response.ok) {
+				response.json().then(data => {
+					this.diceImageLayout = data;
+				});
+			}
 		});
 	}
 }
