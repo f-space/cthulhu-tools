e515d17a0d623bc0cfb8fa27b4914feb2f1db32e
F_ <hn.fspace@outlook.com>
2017-12-13 01:52:46 +0900

    Mixin時の型の定義を変更

--- src/mixins/mixin.ts
+++ src/mixins/mixin.ts
@@ -1,71 +1,72 @@
-import Vue from 'vue';
+import { Vue } from 'vue/types/vue';
 import { ComponentOptions } from 'vue/types/options';
 
-type VueClass<V extends Vue> = { new(...args: any[]): V } & typeof Vue;
+type VueCtor<V extends Vue> = { new(...args: any[]): V };
+type VueClass<V extends Vue> = VueCtor<V> & typeof Vue;
 type MixinType<V extends Vue> = ComponentOptions<V> | VueClass<V>;
 
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue>(
-	Base: VueClass<T>,
-	mixin1: MixinType<M1>): VueClass<T & M1>;
+	Base: T,
+	mixin1: MixinType<M1>): T & VueCtor<M1>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
-	mixin2: MixinType<M2>): VueClass<T & M1 & M2>;
+	mixin2: MixinType<M2>): T & VueCtor<M1 & M2>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue,
 	M3 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
 	mixin2: MixinType<M2>,
-	mixin3: MixinType<M3>): VueClass<T & M1 & M2 & M3>;
+	mixin3: MixinType<M3>): T & VueCtor<M1 & M2 & M3>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue,
 	M3 extends Vue,
 	M4 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
 	mixin2: MixinType<M2>,
 	mixin3: MixinType<M3>,
-	mixin4: MixinType<M4>): VueClass<T & M1 & M2 & M3 & M4>;
+	mixin4: MixinType<M4>): T & VueCtor<M1 & M2 & M3 & M4>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue,
 	M3 extends Vue,
 	M4 extends Vue,
 	M5 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
 	mixin2: MixinType<M2>,
 	mixin3: MixinType<M3>,
 	mixin4: MixinType<M4>,
-	mixin5: MixinType<M5>): VueClass<T & M1 & M2 & M3 & M4 & M5>;
+	mixin5: MixinType<M5>): T & VueCtor<M1 & M2 & M3 & M4 & M5>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue,
 	M3 extends Vue,
 	M4 extends Vue,
 	M5 extends Vue,
 	M6 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
 	mixin2: MixinType<M2>,
 	mixin3: MixinType<M3>,
 	mixin4: MixinType<M4>,
 	mixin5: MixinType<M5>,
-	mixin6: MixinType<M6>): VueClass<T & M1 & M2 & M3 & M4 & M5 & M6>;
+	mixin6: MixinType<M6>): T & VueCtor<M1 & M2 & M3 & M4 & M5 & M6>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue,
 	M3 extends Vue,
@@ -73,16 +74,16 @@ function mixin<
 	M5 extends Vue,
 	M6 extends Vue,
 	M7 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
 	mixin2: MixinType<M2>,
 	mixin3: MixinType<M3>,
 	mixin4: MixinType<M4>,
 	mixin5: MixinType<M5>,
 	mixin6: MixinType<M6>,
-	mixin7: MixinType<M7>): VueClass<T & M1 & M2 & M3 & M4 & M5 & M6 & M7>;
+	mixin7: MixinType<M7>): T & VueCtor<M1 & M2 & M3 & M4 & M5 & M6 & M7>;
 function mixin<
-	T extends Vue,
+	T extends VueClass<Vue>,
 	M1 extends Vue,
 	M2 extends Vue,
 	M3 extends Vue,
@@ -91,7 +92,7 @@ function mixin<
 	M6 extends Vue,
 	M7 extends Vue,
 	M8 extends Vue>(
-	Base: VueClass<T>,
+	Base: T,
 	mixin1: MixinType<M1>,
 	mixin2: MixinType<M2>,
 	mixin3: MixinType<M3>,
@@ -99,7 +100,7 @@ function mixin<
 	mixin5: MixinType<M5>,
 	mixin6: MixinType<M6>,
 	mixin7: MixinType<M7>,
-	mixin8: MixinType<M8>): VueClass<T & M1 & M2 & M3 & M4 & M5 & M6 & M7 & M8>;
+	mixin8: MixinType<M8>): T & VueCtor<M1 & M2 & M3 & M4 & M5 & M6 & M7 & M8>;
 function mixin(Base: VueClass<Vue>, ...mixins: MixinType<Vue>[]) {
 	return Base.extend({ mixins });
 }
