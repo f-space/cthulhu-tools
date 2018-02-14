354aa9043d47fb6b68519fcc762d2bb04aca212d
F_ <hn.fspace@outlook.com>
2018-02-14 21:04:01 +0900

    Vuexストアの削除

--- src/modules/root.ts
+++
@@ -1,8 +0,0 @@
-import { Module, Child } from "modules/vuex-class-module";
-import StatusModule from "modules/status";
-
-@Module
-export default class RootModule {
-	@Child
-	public readonly status!: StatusModule;
-}
