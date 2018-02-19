9840275e3234e9e3eac59dbb55635c1bcf0bfff1
F_ <hn.fspace@outlook.com>
2018-02-19 18:04:55 +0900

    ホームページの実装

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
+import Page from "components/templates/page";
+import HomeContent from "templates/pages/home.pugx";
+import style from "styles/pages/home.scss";
 
-export default function HomePage() { return null; }
+export default function HomePage() {
+	return <Page id={style['home']} heading={<h2>クトゥルフTRPGのための<wbr />Webアプリ</h2>}>
+		<HomeContent {...style} />
+	</Page>
+}
