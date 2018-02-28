7cc1b8ef5c73a6e1dcaf13662c7b3d235e68917e
F_ <hn.fspace@outlook.com>
2018-03-01 04:16:19 +0900

    ナビゲーションアニメーション時のエラーを修正

--- src/components/frame/navigation.tsx
+++ src/components/frame/navigation.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, NavLink, NavLinkProps } from 'react-router-dom';
 import classNames from 'classnames';
 import style from "styles/frame/navigation.scss";
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 export interface NavigationProps extends React.HTMLAttributes<HTMLElement> { }
 export interface NavigationLinkProps extends NavLinkProps { }
@@ -16,16 +15,14 @@ export default function Navigation(props: NavigationProps) {
 	return <nav {...rest} className={classNames(className, style['navigation'])}>
 		<NavigationLink to="/dice">Dice</NavigationLink>
 		<NavigationLink to="/status">Status</NavigationLink>
-		<TransitionGroup component={(props: any) => props.children}>
-			<NavigationIndicator path="/status/character-management">
-				<span>Character</span>
-				<span>Management</span>
-			</NavigationIndicator>
-			<NavigationIndicator path="/status/character-edit">
-				<span>Character</span>
-				<span>Edit</span>
-			</NavigationIndicator>
-		</TransitionGroup>
+		<NavigationIndicator path="/status/character-management">
+			<span>Character</span>
+			<span>Management</span>
+		</NavigationIndicator>
+		<NavigationIndicator path="/status/character-edit">
+			<span>Character</span>
+			<span>Edit</span>
+		</NavigationIndicator>
 	</nav>
 }
 
@@ -41,7 +38,7 @@ export function NavigationLink({ className, to, children, ...rest }: NavigationL
 	</NavLink>
 }
 
-export function NavigationIndicator({ className, path, children, ...rest }: NavigationIndicatorProps) {
+export function NavigationIndicator({ path, className, children, ...rest }: NavigationIndicatorProps) {
 	const classList = [
 		className,
 		style['nav-item'],
@@ -50,9 +47,9 @@ export function NavigationIndicator({ className, path, children, ...rest }: Navi
 		{ [style['two-lines']]: Array.isArray(children) && children.length >= 2 },
 	];
 
-	return <Route exact path={path} render={() =>
-		<CSSTransition classNames="slide" timeout={Infinity}>
-			<div {...rest} className={classNames(classList)}>{children}</div>
-		</CSSTransition>
-	} />
+	return <Route exact path={path} children={({ match }) => {
+		if (!match) classList.push(style['hidden']);
+
+		return <div {...rest} className={classNames(classList)}>{children}</div>
+	}} />
 }
