f507e61a081e2fce27b0274269fe6bd93ee46e66
F_ <hn.fspace@outlook.com>
2018-03-01 04:21:52 +0900

    各コンポーネントでReduxストアにアクセスするための準備

--- src/components/frame/app.tsx
+++ src/components/frame/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Header from "components/frame/header";
 import Navigation from "components/frame/navigation";
 import { DialogProvider, DialogSlot } from "components/frame/dialog";
@@ -8,31 +9,34 @@ import Dice from "components/pages/dice";
 import Status from "components/pages/status";
 import CharacterManagement from "components/pages/character-management";
 import CharacterEdit from "components/pages/character-edit";
+import store from "redux/store";
 import style from "styles/frame/app.scss";
 
 export default class App extends React.Component {
 	public render() {
-		return <HashRouter>
-			<div className={style['app']}>
-				<DialogProvider>
-					<DialogSlot>
-						<div className={style['overlay']} />
-					</DialogSlot>
-					<div className={style['container']}>
-						<Header className={style['header']} />
-						<main className={style['content']}>
-							<Switch>
-								<Route exact path="/" component={Home} />
-								<Route exact path="/dice" component={Dice} />
-								<Route exact path="/status" component={Status} />
-								<Route exact path="/status/character-management" component={CharacterManagement} />
-								<Route exact path="/status/character-edit" component={CharacterEdit} />
-							</Switch>
-						</main>
-						<Navigation className={style['navigation']} />
-					</div>
-				</DialogProvider>
-			</div>
-		</HashRouter>
+		return <Provider store={store}>
+			<HashRouter>
+				<div className={style['app']}>
+					<DialogProvider>
+						<DialogSlot>
+							<div className={style['overlay']} />
+						</DialogSlot>
+						<div className={style['container']}>
+							<Header className={style['header']} />
+							<main className={style['content']}>
+								<Switch>
+									<Route exact path="/" component={Home} />
+									<Route exact path="/dice" component={Dice} />
+									<Route exact path="/status" component={Status} />
+									<Route exact path="/status/character-management" component={CharacterManagement} />
+									<Route exact path="/status/character-edit" component={CharacterEdit} />
+								</Switch>
+							</main>
+							<Navigation className={style['navigation']} />
+						</div>
+					</DialogProvider>
+				</div>
+			</HashRouter>
+		</Provider>
 	}
 }
