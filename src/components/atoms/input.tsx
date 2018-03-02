8d32132b0f31f1258381bcf91db159e2f5edab29
F_ <hn.fspace@outlook.com>
2018-03-02 20:24:34 +0900

    チェックボックスの実装

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -4,9 +4,16 @@ import { Input, InputProps } from "components/functions/form";
 import style from "styles/atoms/input.scss";
 
 export interface NumberInputProps extends InputProps { }
+export interface CheckboxProps extends InputProps { }
 
 export function NumberInput(props: NumberInputProps) {
 	const { className, ...rest } = props;
 
 	return <Input {...rest} className={classNames(className, style['input'], style['number'])} type="number" />
+}
+
+export function Checkbox(props: CheckboxProps) {
+	const { className, ...rest } = props;
+
+	return <Input {...rest} className={classNames(className, style['input'], style['checkbox'])} type="checkbox" />
 }
