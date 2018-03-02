20e0dfa27f9e64782827153e27765d19ab294612
F_ <hn.fspace@outlook.com>
2018-03-02 20:24:54 +0900

    トグルボタンの実装

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -5,6 +5,10 @@ import style from "styles/atoms/input.scss";
 
 export interface NumberInputProps extends InputProps { }
 export interface CheckboxProps extends InputProps { }
+export interface ToggleProps extends InputProps {
+	on: string;
+	off: string;
+}
 
 export function NumberInput(props: NumberInputProps) {
 	const { className, ...rest } = props;
@@ -16,4 +20,10 @@ export function Checkbox(props: CheckboxProps) {
 	const { className, ...rest } = props;
 
 	return <Input {...rest} className={classNames(className, style['input'], style['checkbox'])} type="checkbox" />
+}
+
+export function Toggle(props: ToggleProps) {
+	const { on, off, className, ...rest } = props;
+
+	return <Input {...rest} className={classNames(className, style['input'], style['toggle'])} type="checkbox" data-on={on} data-off={off} />
 }
