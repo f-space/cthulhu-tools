ddc9c29e29a103b7b555ecedbf26204589db676c
F_ <hn.fspace@outlook.com>
2018-06-12 16:46:30 +0900

    プロファイルからバージョン情報を削除

--- src/models/data/profile.ts
+++ src/models/data/profile.ts
@@ -2,7 +2,6 @@ import * as validation from "./validation";
 
 export interface ProfileData {
 	readonly uuid: string;
-	readonly version?: number;
 	readonly name: string;
 	readonly attributes?: ReadonlyArray<string>;
 	readonly skills?: ReadonlyArray<string>;
@@ -10,7 +9,6 @@ export interface ProfileData {
 
 export interface ProfileConfig {
 	readonly uuid: string;
-	readonly version: number;
 	readonly name: string;
 	readonly attributes?: ReadonlyArray<string>;
 	readonly skills?: ReadonlyArray<string>;
@@ -18,25 +16,22 @@ export interface ProfileConfig {
 
 export class Profile {
 	public readonly uuid: string;
-	public readonly version: number;
 	public readonly name: string;
 	public readonly attributes: ReadonlyArray<string>;
 	public readonly skills: ReadonlyArray<string>;
 	public readonly readonly: boolean;
 
-	public constructor({ uuid, version, name, attributes, skills }: ProfileConfig, readonly?: boolean) {
+	public constructor({ uuid, name, attributes, skills }: ProfileConfig, readonly?: boolean) {
 		this.uuid = uuid;
-		this.version = version;
 		this.name = name;
 		this.attributes = attributes !== undefined ? attributes : [];
 		this.skills = skills !== undefined ? skills : [];
 		this.readonly = Boolean(readonly);
 	}
 
-	public static from({ uuid, version, name, attributes, skills }: ProfileData, readonly?: boolean) {
+	public static from({ uuid, name, attributes, skills }: ProfileData, readonly?: boolean) {
 		return new Profile({
 			uuid: validation.uuid(uuid),
-			version: validation.int(validation.or(version, 1), 1),
 			name: validation.string(name),
 			attributes: validation.array(attributes, validation.string),
 			skills: validation.array(skills, validation.string),
@@ -46,7 +41,6 @@ export class Profile {
 	public toJSON(): ProfileData {
 		return {
 			uuid: this.uuid,
-			version: this.version,
 			name: this.name,
 			attributes: this.attributes.length !== 0 ? this.attributes : undefined,
 			skills: this.skills.length !== 0 ? this.skills : undefined,
@@ -54,8 +48,8 @@ export class Profile {
 	}
 
 	public set(config: Partial<ProfileConfig>, readonly?: boolean): Profile {
-		const { uuid, version, name, attributes, skills } = this;
+		const { uuid, name, attributes, skills } = this;
 
-		return new Profile({ uuid, version, name, attributes, skills, ...config }, readonly);
+		return new Profile({ uuid, name, attributes, skills, ...config }, readonly);
 	}
 }
