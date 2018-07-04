a6f1f3852354c075f19f46b544a98952f53878ea
F_ <hn.fspace@outlook.com>
2018-07-04 19:03:13 +0900

    不要になった関数の削除

--- src/models/utility.ts
+++ src/models/utility.ts
@@ -1,30 +1,9 @@
-export function deepClone<T>(value: T): T {
-	return JSON.parse(JSON.stringify(value), (k, v) => {
-		if (typeof v === 'object' && v !== null && !Array.isArray(v)) {
-			return Object.assign(Object.create(null), v);
-		}
-		return v;
-	});
-}
-
 export function generateUUID(): string {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, ch => {
 		const random = Math.random() * 16 | 0;
 		const value = (ch === 'x' ? random : (random & 0x03 | 0x08));
 		return value.toString(16);
 	});
-}
-
-export function getFNV1a(data: string): number {
-	let hash = 2166136261;
-	for (let i = 0, len = data.length; i < len; i++) {
-		const bytes = data.charCodeAt(i);
-		hash ^= (bytes >>> 8);
-		hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
-		hash ^= (bytes & 0xff);
-		hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24);
-	}
-	return (hash >>> 0);
 }
 
 export function throttle<T extends Function>(interval: number, fn: T): T {
