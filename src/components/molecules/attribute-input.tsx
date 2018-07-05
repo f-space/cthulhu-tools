27033e60167c8d325311995ab94cc65e38edcc1e
F_ <hn.fspace@outlook.com>
2018-07-05 21:00:12 +0900

    式周りのリファクタリング

--- src/components/molecules/attribute-input.tsx
+++ src/components/molecules/attribute-input.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Expression, AttributeType, Attribute, InputType, InputMethod } from "models/status";
+import { AST, Variable, Expression, AttributeType, Attribute, InputType, InputMethod } from "models/status";
 import { EvaluationText } from "components/atoms/evaluation-text";
 import { AttributeDiceInput } from "components/molecules/attribute-dice-input";
 import { AttributeNumberInput } from "components/molecules/attribute-number-input";
@@ -43,7 +43,7 @@ export class AttributeInput extends React.PureComponent<AttributeInputProps> {
 
 	private segment(attribute: Attribute): (string | InputMethod)[] {
 		const expression = getExpression(attribute);
-		const segments = expression ? expression.segment() : [];
+		const segments = expression ? (new ASTSegmenter()).segment(expression.ast) : [];
 		return segments
 			.map(x => typeof x === 'string' ? x : attribute.inputs.find(input => input.name === x.name))
 			.filter(x => x) as (string | InputMethod)[];
@@ -55,5 +55,106 @@ export class AttributeInput extends React.PureComponent<AttributeInputProps> {
 				case AttributeType.Text: return attribute.expression;
 			}
 		}
+	}
+}
+
+class ASTSegmenter {
+	public segment(node: AST.Node): (string | Variable)[] {
+		function* flatten(segments: any[]): IterableIterator<string | Variable> {
+			for (const segment of segments) {
+				if (Array.isArray(segment)) {
+					yield* flatten(segment);
+				} else {
+					yield segment;
+				}
+			}
+		}
+
+		function* join(segments: IterableIterator<string | Variable>) {
+			let chunk = [];
+			for (const segment of segments) {
+				if (typeof segment === 'string') {
+					chunk.push(segment);
+				} else {
+					if (chunk.length > 0) yield chunk.join("");
+					yield segment;
+					chunk = [];
+				}
+			}
+			if (chunk.length > 0) yield chunk.join("");
+		}
+
+		return Array.from(join(flatten(this.visit(node))));
+	}
+
+	protected visit(node: AST.Node): any {
+		switch (node.type) {
+			case AST.NodeType.BinaryOp:
+				return this.onBinaryOperator(node);
+			case AST.NodeType.UnaryOp:
+				return this.onUnaryOperator(node);
+			case AST.NodeType.FunctionCall:
+				return this.onFunctionCall(node);
+			case AST.NodeType.Literal:
+				return this.onLiteral(node);
+			case AST.NodeType.Template:
+				return this.onTemplate(node);
+			case AST.NodeType.Substitution:
+				return this.onSubstitution(node);
+			case AST.NodeType.Text:
+				return this.onText(node);
+			case AST.NodeType.Variable:
+				return this.onVariable(node);
+			case AST.NodeType.Reference:
+				return this.onReference(node);
+		}
+	}
+
+	protected onBinaryOperator(node: AST.BinaryOperator): any[] {
+		const lhs = this.visit(node.lhs);
+		const rhs = this.visit(node.rhs);
+		const lhsSeg = node.priority < node.lhs.priority ? ["(", lhs, ")"] : lhs;
+		const rhsSeg = node.priority > node.rhs.priority ? rhs : ["(", rhs, ")"];
+		return [lhsSeg, " ", node.op, " ", rhsSeg];
+	}
+
+	protected onUnaryOperator(node: AST.UnaryOperator): any[] {
+		const expression = this.visit(node.expression);
+		const exprSeg = node.priority < node.expression.priority ? ["(", expression, ")"] : expression;
+		return [node.op, exprSeg];
+	}
+
+	protected onFunctionCall(node: AST.FunctionCall): any[] {
+		const args = node.args.map(x => this.visit(x));
+		const argsSeg = args.slice(0, 1).concat(args.slice(1).map(arg => [", ", arg]));
+		return [node.fn, "(", argsSeg, ")"];
+	}
+
+	protected onLiteral(node: AST.Literal): any[] {
+		return [String(node)];
+	}
+
+	protected onTemplate(node: AST.Template): any[] {
+		const segments = node.segments.map(x => this.visit(x));
+
+		return segments;
+	}
+
+	protected onSubstitution(node: AST.Substitution): any[] {
+		const expression = this.visit(node.expression);
+
+		return expression;
+	}
+
+	protected onText(node: AST.Text): any[] {
+		return [String(node)];
+	}
+
+	protected onVariable(node: AST.Variable): any[] {
+		return [new Variable(node.name)];
+	}
+
+	protected onReference(node: AST.Reference): any[] {
+		return [String(node)];
 	}
 }
--- src/models/data/expression.ts
+++ src/models/data/expression.ts
@@ -2,6 +2,8 @@ import * as AST from "./ast";
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
+export { AST };
+
 export class Variable {
 	public readonly key: string;
 
@@ -53,51 +55,21 @@ export class Reference {
 	}
 }
 
-function collectDependencies(ast: AST.Node): { vars: Variable[], refs: Reference[] } {
-	const vars = new Map<string, Variable>();
-	const refs = new Map<string, Reference>();
-	const visitor = (node: AST.Node) => {
-		switch (node.type) {
-			case AST.NodeType.Variable:
-				const variable = new Variable(node.name);
-				if (!vars.has(variable.key)) vars.set(variable.key, variable);
-				break;
-			case AST.NodeType.Reference:
-				const reference = new Reference(node.id, node.modifier, node.scope);
-				if (!refs.has(reference.key)) refs.set(reference.key, reference);
-				break;
-		}
-
-		node.visitChildren(visitor);
-	}
-	visitor(ast);
-
-	return { vars: [...vars.values()], refs: [...refs.values()] };
-}
-
 export class Expression {
 	protected constructor(readonly ast: AST.Node, readonly vars: ReadonlyArray<Variable>, readonly refs: ReadonlyArray<Reference>) { }
-
-	public static value(value: number): Expression {
-		return new Expression(new AST.Literal(value), [], []);
-	}
 
 	public static parse(source: string): Expression | null {
 		try {
 			const ast = AST.parse(source);
-			const deps = collectDependencies(ast);
+			const deps = this.collectDependencies(ast);
 			return new Expression(ast, deps.vars, deps.refs);
 		} catch {
 			return null;
 		}
 	}
 
-	public evaluate(values?: Map<string, any>): number | undefined {
-		return (new ASTEvaluator(values)).evaluate(this.ast) as number | undefined;
-	}
-
-	public segment(): (string | Variable)[] {
-		return (new ASTSegmenter()).segment(this.ast);
+	public evaluate(values?: Map<string, any>): any {
+		return ASTEvaluator.evaluate(this.ast, values);
 	}
 
 	public toJSON(): string {
@@ -107,10 +79,40 @@ export class Expression {
 	public toString(): string {
 		return this.ast.toString();
 	}
+
+	private static collectDependencies(ast: AST.Node): { vars: Variable[], refs: Reference[] } {
+		const vars = new Map<string, Variable>();
+		const refs = new Map<string, Reference>();
+		const visitor = (node: AST.Node) => {
+			switch (node.type) {
+				case AST.NodeType.Variable:
+					const variable = new Variable(node.name);
+					if (!vars.has(variable.key)) vars.set(variable.key, variable);
+					break;
+				case AST.NodeType.Reference:
+					const reference = new Reference(node.id, node.modifier, node.scope);
+					if (!refs.has(reference.key)) refs.set(reference.key, reference);
+					break;
+			}
+
+			node.visitChildren(visitor);
+		};
+		visitor(ast);
+
+		return { vars: [...vars.values()], refs: [...refs.values()] };
+	}
 }
 
-abstract class ASTVisitor {
-	protected visit(node: AST.Node): any {
+class ASTEvaluator {
+	public constructor(readonly values: Map<string, any> = new Map()) { }
+
+	public static evaluate(node: AST.Node, values?: Map<string, any>): any {
+		const evaluator = new ASTEvaluator(values);
+
+		return evaluator.evaluate(node);
+	}
+
+	protected evaluate(node: AST.Node): any {
 		switch (node.type) {
 			case AST.NodeType.BinaryOp:
 				return this.onBinaryOperator(node);
@@ -133,29 +135,9 @@ abstract class ASTVisitor {
 		}
 	}
 
-	protected abstract onBinaryOperator(node: AST.BinaryOperator): any;
-	protected abstract onUnaryOperator(node: AST.UnaryOperator): any;
-	protected abstract onFunctionCall(node: AST.FunctionCall): any;
-	protected abstract onLiteral(node: AST.Literal): any;
-	protected abstract onVariable(node: AST.Variable): any;
-	protected abstract onReference(node: AST.Reference): any;
-	protected abstract onTemplate(node: AST.Template): any;
-	protected abstract onSubstitution(node: AST.Substitution): any;
-	protected abstract onText(node: AST.Text): any;
-}
-
-class ASTEvaluator extends ASTVisitor {
-	public constructor(readonly values: Map<string, number | string | undefined> = new Map()) {
-		super();
-	}
-
-	public evaluate(node: AST.Node): number | string | undefined {
-		return this.visit(node);
-	}
-
-	protected onBinaryOperator(node: AST.BinaryOperator): number | undefined {
-		const lhs = this.visit(node.lhs);
-		const rhs = this.visit(node.rhs);
+	protected onBinaryOperator(node: AST.BinaryOperator): any {
+		const lhs = this.evaluate(node.lhs);
+		const rhs = this.evaluate(node.rhs);
 		if (lhs === undefined || rhs === undefined) return undefined;
 
 		switch (node.op) {
@@ -173,8 +155,8 @@ class ASTEvaluator extends ASTVisitor {
 		}
 	}
 
-	protected onUnaryOperator(node: AST.UnaryOperator): number | undefined {
-		const expression = this.visit(node.expression);
+	protected onUnaryOperator(node: AST.UnaryOperator): any {
+		const expression = this.evaluate(node.expression);
 		if (expression === undefined) return undefined;
 
 		switch (node.op) {
@@ -183,8 +165,8 @@ class ASTEvaluator extends ASTVisitor {
 		}
 	}
 
-	protected onFunctionCall(node: AST.FunctionCall): number | string | undefined {
-		const args = node.args.map(x => this.visit(x));
+	protected onFunctionCall(node: AST.FunctionCall): any {
+		const args = node.args.map(x => this.evaluate(x));
 		if (args.some(arg => arg === undefined)) return undefined;
 
 		switch (node.fn) {
@@ -199,111 +181,33 @@ class ASTEvaluator extends ASTVisitor {
 		}
 	}
 
-	protected onLiteral(node: AST.Literal): number {
+	protected onLiteral(node: AST.Literal): any {
 		return node.value;
 	}
 
-	protected onTemplate(node: AST.Template): string | undefined {
-		const segments = node.segments.map(x => this.visit(x));
+	protected onTemplate(node: AST.Template): any {
+		const segments = node.segments.map(x => this.evaluate(x));
 		if (segments.some(seg => seg === undefined)) return undefined;
 
 		return segments.join("");
 	}
 
-	protected onSubstitution(node: AST.Substitution): string | undefined {
-		const expression = this.visit(node.expression);
+	protected onSubstitution(node: AST.Substitution): any {
+		const expression = this.evaluate(node.expression);
 		if (expression === undefined) return undefined;
 
 		return String(expression);
 	}
 
-	protected onText(node: AST.Text): string {
+	protected onText(node: AST.Text): any {
 		return node.value;
 	}
 
-	protected onVariable(node: AST.Variable): number | string | undefined {
+	protected onVariable(node: AST.Variable): any {
 		return this.values.get(Variable.key(node.name));
 	}
 
-	protected onReference(node: AST.Reference): number | string | undefined {
+	protected onReference(node: AST.Reference): any {
 		return this.values.get(Reference.key(node.id, node.modifier, node.scope));
-	}
-}
-
-class ASTSegmenter extends ASTVisitor {
-	public segment(node: AST.Node): (string | Variable)[] {
-		function* flatten(segments: any[]): IterableIterator<string | Variable> {
-			for (const segment of segments) {
-				if (Array.isArray(segment)) {
-					yield* flatten(segment);
-				} else {
-					yield segment;
-				}
-			}
-		}
-
-		function* join(segments: IterableIterator<string | Variable>) {
-			let chunk = [];
-			for (const segment of segments) {
-				if (typeof segment === 'string') {
-					chunk.push(segment);
-				} else {
-					if (chunk.length > 0) yield chunk.join("");
-					yield segment;
-					chunk = [];
-				}
-			}
-			if (chunk.length > 0) yield chunk.join("");
-		}
-
-		return Array.from(join(flatten(this.visit(node))));
-	}
-
-	protected onBinaryOperator(node: AST.BinaryOperator): any[] {
-		const lhs = this.visit(node.lhs);
-		const rhs = this.visit(node.rhs);
-		const lhsSeg = node.priority < node.lhs.priority ? ["(", lhs, ")"] : lhs;
-		const rhsSeg = node.priority > node.rhs.priority ? rhs : ["(", rhs, ")"];
-		return [lhsSeg, " ", node.op, " ", rhsSeg];
-	}
-
-	protected onUnaryOperator(node: AST.UnaryOperator): any[] {
-		const expression = this.visit(node.expression);
-		const exprSeg = node.priority < node.expression.priority ? ["(", expression, ")"] : expression;
-		return [node.op, exprSeg];
-	}
-
-	protected onFunctionCall(node: AST.FunctionCall): any[] {
-		const args = node.args.map(x => this.visit(x));
-		const argsSeg = args.slice(0, 1).concat(args.slice(1).map(arg => [", ", arg]));
-		return [node.fn, "(", argsSeg, ")"];
-	}
-
-	protected onLiteral(node: AST.Literal): any[] {
-		return [String(node)];
-	}
-
-	protected onTemplate(node: AST.Template): any[] {
-		const segments = node.segments.map(x => this.visit(x));
-
-		return segments;
-	}
-
-	protected onSubstitution(node: AST.Substitution): any[] {
-		const expression = this.visit(node.expression);
-
-		return expression;
-	}
-
-	protected onText(node: AST.Text): any[] {
-		return [String(node)];
-	}
-
-	protected onVariable(node: AST.Variable): any[] {
-		return [new Variable(node.name)];
-	}
-
-	protected onReference(node: AST.Reference): any[] {
-		return [String(node)];
 	}
 }
