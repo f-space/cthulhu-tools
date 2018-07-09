9654890f0840c0dbfc435e9549e0ac232ec9cf29
F_ <hn.fspace@outlook.com>
2018-07-10 04:12:35 +0900

    属性入力コンポーネントのリファクタリング

--- src/components/molecules/attribute-input/expression-arranger.ts
+++ src/components/molecules/attribute-input/expression-arranger.ts
@@ -1,67 +1,12 @@
-import React from 'react';
-import classNames from 'classnames';
-import { AST, Reference, AttributeType, Attribute, InputType, InputMethod, PropertyResolver } from "models/status";
-import { EvaluationConsumer } from "components/functions/evaluation";
-import { EvaluationText } from "components/atoms/evaluation-text";
-import { AttributeDiceInput } from "components/molecules/attribute-dice-input";
-import { AttributeNumberInput } from "components/molecules/attribute-number-input";
-import { AttributeTextInput } from "components/molecules/attribute-text-input";
-import style from "styles/molecules/attribute-input.scss";
+import { AST, Reference, AttributeType, Attribute, PropertyResolver } from "models/status";
 
-export interface AttributeInputProps extends React.HTMLAttributes<HTMLElement> {
-	name: string;
-	attribute: Attribute;
-}
-
-export class AttributeInput extends React.PureComponent<AttributeInputProps> {
-	public render() {
-		const { name, attribute, className, ...rest } = this.props;
-
-		return <div {...rest} className={classNames(className, style['attribute'])}>
-			<div className={style['name']}>{attribute.name}</div>
-			<div className={style['input']}>
-				<EvaluationConsumer>
-					{chain => {
-						const renderer = new ASTRenderer(chain.resolver);
-						const segments = renderer.render(attribute);
-						return segments.map((segment, i) => {
-							if (i % 2 === 0) {
-								return segment;
-							} else {
-								const input = attribute.inputs.find(input => input.name === segment);
-								return input ? this.renderInput(input) : null;
-							}
-						});
-					}}
-				</EvaluationConsumer>
-			</div>
-			<div className={style['value']}>
-				<EvaluationText expression={attribute.id} hash={null} />
-			</div>
-		</div>
-	}
-
-	private renderInput(method: InputMethod): React.ReactNode {
-		const props = {
-			key: method.name,
-			name: `${this.props.name}.${method.name}`,
-		}
-
-		switch (method.type) {
-			case InputType.Dice: return <AttributeDiceInput {...props} method={method} />
-			case InputType.Number: return <AttributeNumberInput {...props} method={method} />
-			case InputType.Text: return <AttributeTextInput {...props} method={method} />
-		}
-	}
-}
-
-class ASTRenderer {
+export class ExpressionArranger {
 	private context!: Attribute;
 	private depth!: number;
 
 	public constructor(readonly resolver: PropertyResolver) { }
 
-	public render(attribute: Attribute): string[] {
+	public arrange(attribute: Attribute): string[] {
 		this.context = attribute;
 		this.depth = 0;
 
