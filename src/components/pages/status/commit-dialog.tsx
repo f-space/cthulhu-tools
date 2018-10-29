4102162d340da3b198790100bc18c0e0c3a9b247
F_ <hn.fspace@outlook.com>
2018-10-29 20:25:02 +0900

    プロパティ変更時の式パースエラーの処理を変更

--- src/components/pages/status/commit-dialog.tsx
+++ src/components/pages/status/commit-dialog.tsx
@@ -31,59 +31,59 @@ export class CommitDialog extends React.Component<CommitDialogProps> {
 		return <Dialog open={open} header={"Commit"}>
 			{
 				() => <Form initialValues={{ mode: 'set' }} onSubmit={this.handleSubmit} render={({ handleSubmit, invalid }) =>
-				<form onSubmit={handleSubmit}>
-					<div className={classNames(style['inputs'], style[target.type])}>
-						<div className={style['name']}>{target.name}</div>
-						<div className={style['methods']}>
-							<div className={classNames(style['method'], style['set'])}>
-								<div className={style['caption']}>
-									<Radio field="mode" value="set" />
-									<span>代入</span>
+					<form onSubmit={handleSubmit}>
+						<div className={classNames(style['inputs'], style[target.type])}>
+							<div className={style['name']}>{target.name}</div>
+							<div className={style['methods']}>
+								<div className={classNames(style['method'], style['set'])}>
+									<div className={style['caption']}>
+										<Radio field="mode" value="set" />
+										<span>代入</span>
+									</div>
+									<div className={style['value']}>
+										<Field name="mode" subscription={{ value: true }} render={({ input: { value } }) =>
+											this.renderInput(target, "set.value", value !== 'set')
+										} />
+									</div>
 								</div>
-								<div className={style['value']}>
-									<Field name="mode" subscription={{ value: true }} render={({ input: { value } }) =>
-										this.renderInput(target, "set.value", value !== 'set')
-									} />
+								<div className={classNames(style['method'], style['add'])}>
+									<div className={style['caption']}>
+										<Radio field="mode" value="add" />
+										<span>加算</span>
+									</div>
+									<div className={style['value']}>
+										<Field name="mode" subscription={{ value: true }} render={({ input: { value } }) =>
+											this.renderInput(target, "add.value", value !== 'add')
+										} />
+									</div>
+								</div>
+								<div className={classNames(style['method'], style['expression'])}>
+									<div className={style['caption']}>
+										<Radio field="mode" value="expression" />
+										<span>式</span>
+										<span className={style['note']}>$_ : 現在値</span>
+									</div>
+									<div className={style['value']}>
+										<Field name="mode" subscription={{ value: true }} render={({ input: { value } }) =>
+											<TextInput field={{
+												name: "expression.value",
+												validate: value => Expression.parse(value) ? undefined : "無効な式",
+											}} disabled={value !== 'expression'} required />
+										} />
+									</div>
 								</div>
 							</div>
-							<div className={classNames(style['method'], style['add'])}>
-								<div className={style['caption']}>
-									<Radio field="mode" value="add" />
-									<span>加算</span>
-								</div>
-								<div className={style['value']}>
-									<Field name="mode" subscription={{ value: true }} render={({ input: { value } }) =>
-										this.renderInput(target, "add.value", value !== 'add')
-									} />
-								</div>
-							</div>
-							<div className={classNames(style['method'], style['expression'])}>
-								<div className={style['caption']}>
-									<Radio field="mode" value="expression" />
-									<span>式</span>
-									<span className={style['note']}>$_ : 現在値</span>
-								</div>
-								<div className={style['value']}>
-									<Field name="mode" subscription={{ value: true }} render={({ input: { value } }) =>
-										<TextInput field={{
-											name: "expression.value",
-											validate: value => Expression.parse(value) ? undefined : "無効な式",
-										}} disabled={value !== 'expression'} required />
-									} />
-								</div>
+							<div className={style['message']}>
+								<span>メッセージ</span>
+								<TextInput field="message" autoComplete="off" />
 							</div>
 						</div>
-						<div className={style['message']}>
-							<span>メッセージ</span>
-							<TextInput field="message" autoComplete="off" />
-						</div>
-					</div>
-					<div className={style['buttons']}>
-						<SubmitButton className={style['ok']} disabled={invalid} commit>OK</SubmitButton>
-						<Button className={style['cancel']} onClick={this.handleClick}>Cancel</Button>
-					</div >
-				</form>
-			} />
+						<div className={style['buttons']}>
+							<SubmitButton className={style['ok']} disabled={invalid} commit>OK</SubmitButton>
+							<Button className={style['cancel']} onClick={this.handleClick}>Cancel</Button>
+						</div >
+					</form>
+				} />
 			}
 		</Dialog >
 	}
@@ -129,27 +129,29 @@ export class CommitDialog extends React.Component<CommitDialogProps> {
 
 	private createSetOperation(target: string, values: any): Operation {
 		const value = values.value;
+		const expression = typeof value === 'number'
+			? Expression.parse(String(Number.isFinite(value) ? value : 0))
+			: Expression.parse(`"${String(value).replace(/[{}`\\]/g, "\\$1")}"`);
 
-		let expression: Expression;
-		if (typeof value === 'number') {
-			expression = Expression.parse(String(Number.isFinite(value) ? value : 0))!;
-		} else {
-			expression = Expression.parse(`"${String(value).replace(/[{}`\\]/g, "\\$1")}"`)!;
-		}
+		if (!expression) throw new Error("unreachable code.");
 
 		return new Operation({ target, value: expression });
 	}
 
 	private createAddOperation(target: string, values: any): Operation {
 		const value = values.value;
-		const expression = Expression.parse(`$_+${Number.isFinite(value) ? value : 0}`)!;
+		const expression = Expression.parse(`$_+${Number.isFinite(value) ? value : 0}`);
+
+		if (!expression) throw new Error("unreachable code.");
 
 		return new Operation({ target, value: expression });
 	}
 
 	private createExpressionOperation(target: string, values: any): Operation {
 		const value = values.value;
-		const expression = Expression.parse(value)!;
+		const expression = Expression.parse(value);
+
+		if (!expression) throw new Error("unreachable code.");
 
 		return new Operation({ target, value: expression });
 	}
